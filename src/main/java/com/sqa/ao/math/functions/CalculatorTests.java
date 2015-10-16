c7c46bbc0ba06a763faf03dd8afa00383f4ae908
TonyKag <tonykag@mail.com>
2015-10-15 20:44:59 -0700

    Before class modified

--- src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
+++ src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
@@ -10,8 +10,6 @@
 
 package com.sqa.ao.math.functions;
 
-import static org.junit.Assert.fail;
-
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -43,9 +41,32 @@ public class CalculatorTests {
 		String[] expAddResults = props.getProperty("expAddResult").split(",");
 		String[] expSubResults = props.getProperty("expSubResult").split(",");
 		String[] expMultResults = props.getProperty("expMultResult").split(",");
-		System.out.println(
-				Arrays.toString(operands1) + "\n" + Arrays.toString(operands2) + "\n" + Arrays.toString(expAddResults)
-						+ "\n" + Arrays.toString(expSubResults) + "\n" + Arrays.toString(expMultResults));
+		double[] operands1D = new double[operands1.length];
+		double[] operands2D = new double[operands2.length];
+		double[] expAddResultsD = new double[expAddResults.length];
+		double[] expSubResultsD = new double[expSubResults.length];
+		double[] expMultResultsD = new double[expMultResults.length];
+		System.out.println("Strings:" + Arrays.toString(operands1) + "\n" + Arrays.toString(operands2) + "\n"
+				+ Arrays.toString(expAddResults) + "\n" + Arrays.toString(expSubResults) + "\n"
+				+ Arrays.toString(expMultResults));
+		for (int i = 0; i < operands1.length; i++) {
+			operands1D[i] = Double.parseDouble(operands1[i]);
+		}
+		for (int i = 0; i < operands2.length; i++) {
+			operands2D[i] = Double.parseDouble(operands2[i]);
+		}
+		for (int i = 0; i < expAddResults.length; i++) {
+			expAddResultsD[i] = Double.parseDouble(expAddResults[i]);
+		}
+		for (int i = 0; i < expSubResults.length; i++) {
+			expSubResultsD[i] = Double.parseDouble(expSubResults[i]);
+		}
+		for (int i = 0; i < expMultResults.length; i++) {
+			expMultResultsD[i] = Double.parseDouble(expMultResults[i]);
+		}
+		System.out.println("Doubles:" + Arrays.toString(operands1D) + "\n" + Arrays.toString(operands2D) + "\n"
+				+ Arrays.toString(expAddResultsD) + "\n" + Arrays.toString(expSubResultsD) + "\n"
+				+ Arrays.toString(expMultResultsD));
 	}
 
 	private double[][] data;
@@ -55,42 +76,42 @@ public class CalculatorTests {
 
 	@Test
 	public void test1() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test2() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test3() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test4() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test5() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test6() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test7() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void test8() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
