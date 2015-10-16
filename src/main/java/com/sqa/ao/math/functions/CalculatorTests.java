36519daedee0aea56a9775066ad0a1ae0bc25bc7
TonyKag <tonykag@mail.com>
2015-10-15 20:30:44 -0700

    @BeforeClass modified

--- src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
+++ src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
@@ -12,6 +12,7 @@ package com.sqa.ao.math.functions;
 
 import static org.junit.Assert.fail;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.junit.BeforeClass;
@@ -35,8 +36,16 @@ public class CalculatorTests {
 
 	@BeforeClass
 	public static void setupClass() {
-		System.out.println("Setup before class");
+		System.out.println("Setup Before Class");
 		Properties props = Helper.loadProperties("calculate.properties");
+		String[] operands1 = props.getProperty("operand1").split(",");
+		String[] operands2 = props.getProperty("operand2").split(",");
+		String[] expAddResults = props.getProperty("expAddResult").split(",");
+		String[] expSubResults = props.getProperty("expSubResult").split(",");
+		String[] expMultResults = props.getProperty("expMultResult").split(",");
+		System.out.println(
+				Arrays.toString(operands1) + "\n" + Arrays.toString(operands2) + "\n" + Arrays.toString(expAddResults)
+						+ "\n" + Arrays.toString(expSubResults) + "\n" + Arrays.toString(expMultResults));
 	}
 
 	private double[][] data;
