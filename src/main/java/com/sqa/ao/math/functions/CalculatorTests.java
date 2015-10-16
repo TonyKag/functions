cef45abd0069c6ba232b6bc316bc051e4fbf75f4
TonyKag <tonykag@mail.com>
2015-10-15 19:24:39 -0700

    junit test case created

---
+++ src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
@@ -0,0 +1,36 @@
+/**
+ *   File Name: CalculatorTests.java<br>
+ *
+ *   Yutaka<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Oct 15, 2015
+ *   
+ */
+
+package com.sqa.ao.math.functions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * CalculatorTests //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class CalculatorTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
