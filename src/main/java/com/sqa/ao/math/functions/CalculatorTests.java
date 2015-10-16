3d2b3ac4d321f1acefab0f3289d57881d114ad41
TonyKag <tonykag@mail.com>
2015-10-15 21:36:54 -0700

    Before class modified. Arrays

--- src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
+++ src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
@@ -36,6 +36,11 @@ public class CalculatorTests {
 
 	private static double test;
 
+	static int columnsCount = 5;
+	static int columnsNumber;
+
+	static int currentTestIndex;
+
 	@BeforeClass
 	public static void setupClass() {
 		System.out.println("Setup Before Class");
@@ -50,9 +55,11 @@ public class CalculatorTests {
 		double[] expAddResultsD = new double[expAddResults.length];
 		double[] expSubResultsD = new double[expSubResults.length];
 		double[] expMultResultsD = new double[expMultResults.length];
-		System.out.println("Strings:" + Arrays.toString(operands1) + "\n" + Arrays.toString(operands2) + "\n"
-				+ Arrays.toString(expAddResults) + "\n" + Arrays.toString(expSubResults) + "\n"
-				+ Arrays.toString(expMultResults));
+		// System.out.println("Strings:" + Arrays.toString(operands1) + "\n" +
+		// Arrays.toString(operands2) + "\n"
+		// + Arrays.toString(expAddResults) + "\n" +
+		// Arrays.toString(expSubResults) + "\n"
+		// + Arrays.toString(expMultResults));
 		for (int i = 0; i < operands1.length; i++) {
 			operands1D[i] = Double.parseDouble(operands1[i]);
 		}
@@ -68,20 +75,54 @@ public class CalculatorTests {
 		for (int i = 0; i < expMultResults.length; i++) {
 			expMultResultsD[i] = Double.parseDouble(expMultResults[i]);
 		}
-		System.out.println("Doubles:" + Arrays.toString(operands1D) + "\n" + Arrays.toString(operands2D) + "\n"
-				+ Arrays.toString(expAddResultsD) + "\n" + Arrays.toString(expSubResultsD) + "\n"
-				+ Arrays.toString(expMultResultsD));
+		// System.out.println("Doubles:" + Arrays.toString(operands1D) + "\n" +
+		// Arrays.toString(operands2D) + "\n"
+		// + Arrays.toString(expAddResultsD) + "\n" +
+		// Arrays.toString(expSubResultsD) + "\n"
+		// + Arrays.toString(expMultResultsD));
 
 		data = new double[operands1D.length][];
-		data[0] = operands1D;
-		data[1] = operands2D;
-		data[2] = expAddResultsD;
-		data[3] = expSubResultsD;
-		data[4] = expMultResultsD;
-		System.out.println("Data: " + Arrays.deepToString(data));
-	}
+		// data[0] = operands1D;
+		// data[1] = operands2D;
+		// data[2] = expAddResultsD;
+		// data[3] = expSubResultsD;
+		// data[4] = expMultResultsD;
+		// System.out.println("Data: " + Arrays.deepToString(data));
+		//
+		double data[][] = { { operands1D[0], operands2D[0], expAddResultsD[0], expSubResultsD[0], expMultResultsD[0] },
+				{ operands1D[1], operands2D[1], expAddResultsD[1], expSubResultsD[1], expMultResultsD[1] },
+				{ operands1D[2], operands2D[2], expAddResultsD[2], expSubResultsD[2], expMultResultsD[2] },
+				{ operands1D[3], operands2D[3], expAddResultsD[3], expSubResultsD[3], expMultResultsD[3] },
+				{ operands1D[4], operands2D[4], expAddResultsD[4], expSubResultsD[4], expMultResultsD[4] },
+				{ operands1D[5], operands2D[5], expAddResultsD[5], expSubResultsD[5], expMultResultsD[5] },
+				{ operands1D[6], operands2D[6], expAddResultsD[6], expSubResultsD[6], expMultResultsD[6] },
+				{ operands1D[7], operands2D[7], expAddResultsD[7], expSubResultsD[7], expMultResultsD[7] } };
 
-	int currentTestIndex;
+		// data[0] = new double[] { operands1D[0], operands2D[0],
+		// expAddResultsD[0], expSubResultsD[0], expMultResultsD[0] };
+		// { operands1D[1], operands2D[1], expAddResultsD[1], expSubResultsD[1],
+		// expMultResultsD[1] },
+		// { operands1D[2], operands2D[2], expAddResultsD[2], expSubResultsD[2],
+		// expMultResultsD[2] },
+		// { operands1D[3], operands2D[3], expAddResultsD[3], expSubResultsD[3],
+		// expMultResultsD[3] },
+		// { operands1D[4], operands2D[4], expAddResultsD[4], expSubResultsD[4],
+		// expMultResultsD[4] },
+		// { operands1D[5], operands2D[5], expAddResultsD[5], expSubResultsD[5],
+		// expMultResultsD[5] },
+		// { operands1D[6], operands2D[6], expAddResultsD[6], expSubResultsD[6],
+		// expMultResultsD[6] },
+		// { operands1D[7], operands2D[7], expAddResultsD[7], expSubResultsD[7],
+		// expMultResultsD[7] } };
+		//
+		data = new double[operands1D.length][];
+		for (int row = 0; row < columnsCount; row++) {
+			data[row] = new double[] { operands1D[row], operands2D[row], expAddResultsD[row], expSubResultsD[row],
+					expMultResultsD[row] };
+		}
+		System.out.println("Data Deep: " + Arrays.deepToString(data));
+
+	}
 
 	@Test
 	public void test1() {
