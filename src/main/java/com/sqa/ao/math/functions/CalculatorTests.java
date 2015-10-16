ca89b36767182cf60dee27f86000d631c9fc6f1c
TonyKag <tonykag@mail.com>
2015-10-15 19:53:00 -0700

    array for tests added. 8 emty tcs added

--- src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
+++ src/main/java/com/sqa/ao/math/functions/CalculatorTests.java
@@ -10,7 +10,7 @@
 
 package com.sqa.ao.math.functions;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -21,15 +21,54 @@ import org.junit.Test;
  * <p>
  * //ADDD (description of core methods)
  * 
- * @author      Yutaka
- * @version     1.0.0
- * @since       1.0
+ * @author Yutaka
+ * @version 1.0.0
+ * @since 1.0
  *
  */
 public class CalculatorTests {
 
+	private double[][] data;
+	int currentTestIndex;
+	double test;
+
 	@Test
-	public void test() {
+	public void test1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test5() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test6() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test7() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test8() {
 		fail("Not yet implemented");
 	}
 
